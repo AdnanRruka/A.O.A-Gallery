9a35cb714d8661a387da5baa87e526369712afb4
Omar haadi <omarhaadi98@gmail.com>
2019-12-17 16:31:58 +0100

    added file

---
+++ indexO.js
@@ -0,0 +1,7 @@
+let myInput = document.createElement("input");
+imgDiv.appendChild(myInput);
+
+let myButton = document.createElement("button");
+myButton.innerHTML = "submit";
+
+imgDiv.appendChild(myButton);
